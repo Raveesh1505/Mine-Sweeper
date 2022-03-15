b23b0ae1750be9db661dfd5094d592aae57012b1
Raveesh Yadav <raveeshyadav8@outlook.com>
2022-03-15 21:28:18 +0530

    Main game

---
+++ src/game/mineSweeper.java
@@ -0,0 +1,17 @@
+/*
+    Classic Mine Sweeper game made using Java.
+    Mine sweeper will be played on a 5x5 game
+    board.
+
+    Author : @Raveesh1505
+*/
+
+package game;
+
+public class mineSweeper {
+    public static void main (String[] args){
+        
+        message.gameStartFirstMessage();
+        gameSource.startMatch();    
+    }
+}
